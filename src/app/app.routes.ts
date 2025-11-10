ba7b30e199defaa0d6d30846fa4f18924d0b8bcd
Cihan Yuksel <cihanyyuksel@gmail.com>
2025-11-10 14:45:25 +0300

    remove unnecessary folders

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { UserList } from './components/user-list/user-list';
-import { UserDetailComponent } from './components/user-detail/user-detail';
-
-export const routes: Routes = [
-  { path: '', component: UserList },
-  { path: 'users/:id', component: UserDetailComponent },
-  { path: '**', redirectTo: '/users' },
-];
